0659739a8f94a6c6c5a4b494dfd4070888866fa7
Raman Malhotra <ramanmalhotra508@gmail.com>
2026-08-30 21:18:06 +0530

    channge

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,6 @@ import react from '@vitejs/plugin-react'
 // VITE_BASE_PATH=/portfolio/ npm run deploy
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH || '/',
+    base: '/portfolio/',
+
 })
